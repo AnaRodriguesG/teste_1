ffe7915ee16abd3c0afc71867b5277281284699a
Adrian <studioamusic@bol.com.br>
2024-06-11 14:00:21 -0300

    1

--- js/script.js
+++ js/script.js
@@ -1,28 +1,25 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+document.addEventListener("DOMContentLoaded", function() {
+    var loginForm = document.getElementById("loginForm");
+    if (loginForm) {
+        loginForm.addEventListener("submit", function(event) {
+            event.preventDefault(); // Evita o comportamento padrão do formulário de ser enviado
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+            // Obtém os valores dos campos de entrada
+            var usernameInput = document.getElementById("username");
+            var passwordInput = document.getElementById("password");
+            var username = usernameInput.value;
+            var password = passwordInput.value;
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+            // Exibe os dados no console do navegador
+            console.log("Usuário:", username);
+            console.log("Senha:", password);
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+            // Limpa os campos de entrada
+            usernameInput.value = "";
+            passwordInput.value = "";
+
+            window.location.href = "index_2.html";
+        });
+        
+    }
+});
--- js/script_1.js
+++ js/script_1.js
@@ -1,25 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var loginForm = document.getElementById("loginForm");
-    if (loginForm) {
-        loginForm.addEventListener("submit", function(event) {
-            event.preventDefault(); // Evita o comportamento padrão do formulário de ser enviado
+  var loginForm = document.getElementById("loginForm");
+  if (loginForm) {
+      loginForm.addEventListener("submit", function(event) {
+          event.preventDefault(); // Evita o comportamento padrão do formulário de ser enviado
 
-            // Obtém os valores dos campos de entrada
-            var usernameInput = document.getElementById("username");
-            var passwordInput = document.getElementById("password");
-            var username = usernameInput.value;
-            var password = passwordInput.value;
+          // Obtém os valores dos campos de entrada
+          var usernameInput = document.getElementById("username");
+          var EmailInput = document.getElementById("TipoEmail");
+          var nascimentoInput = document.getElementById("nascimento");
+          var celularInput = document.getElementById("celular");
+          var passwordInput = document.getElementById("password");
+          var username = usernameInput.value;
+          var TipoEmail = EmailInput.value;
+          var nascimento = nascimentoInput.value;
+          var celular = celularInput.value;
+          var password = passwordInput.value;
+          
 
-            // Exibe os dados no console do navegador
-            console.log("Usuário:", username);
-            console.log("Senha:", password);
+          // Exibe os dados no console do navegador
+          console.log("Nome:", username);
+          console.log("Email:", TipoEmail);
+          console.log("Data de Nascimneto:", nascimento);
+          console.log("Celular:",celular );
+          console.log("Senha:", password );
 
-            // Limpa os campos de entrada
-            usernameInput.value = "";
-            passwordInput.value = "";
 
-            window.location.href = "index_1.html ";
-        });
-        
-    }
+          // Limpa os campos de entrada
+          usernameInput.value = "";
+          EmailInput.value = "";
+          nascimentoInput.value = "";
+          celularInput.value = "";
+          passwordInput.value = "";
+
+
+
+          window.location.href = "index.html";
+      });
+      
+  }
 });
